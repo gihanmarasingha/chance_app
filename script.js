e29ff9cca91cdd5e8b679c978f4fc53d0fbf3973
Gihan Marasingha <gihan.marasingha@gmail.com>
2023-09-27 00:18:51 +0100

    use sliders

--- script.js
+++ script.js
@@ -11,30 +11,54 @@ function maxRunLength(arr) {
     }
     return Math.max(maxRun, currentRun);
   }
+
+document.getElementById("m").addEventListener("input", function() {
+    document.getElementById("m-output").textContent = this.value;
+  });
+document.getElementById("n").addEventListener("input", function() {
+    document.getElementById("n-output").textContent = this.value;
+});
+
+
+function updateOutputAndGenerateHistogram(sliderID, outputID) {
+    const slider = document.getElementById(sliderID);
+    const output = document.getElementById(outputID);
+    output.textContent = slider.value;
   
-  function generateHistogram() {
+    slider.addEventListener("input", function() {
+      output.textContent = this.value;
+      generateHistogram();  // Regenerate histogram when slider value changes
+    });
+  }
+  
+  // Update output elements when sliders are moved and regenerate histogram
+  updateOutputAndGenerateHistogram("m", "m-output");
+  updateOutputAndGenerateHistogram("n", "n-output");
+  
+
+function generateHistogram() {
     const m = parseInt(document.getElementById("m").value);
     const n = parseInt(document.getElementById("n").value);
     const maxRunLengths = [];
-  
+
     for (let i = 0; i < m; i++) {
-      const arr = Array.from({ length: n }, () => Math.floor(Math.random() * 2));
-      const maxRun = maxRunLength(arr);
-      maxRunLengths.push(maxRun);
+    const arr = Array.from({ length: n }, () => Math.floor(Math.random() * 2));
+    const maxRun = maxRunLength(arr);
+    maxRunLengths.push(maxRun);
     }
-  
+
     const meanMaxRun = maxRunLengths.reduce((a, b) => a + b, 0) / m;
     console.log(`Mean Maximum Run Length: ${meanMaxRun}`);
-  
+
     const trace = {
-      x: maxRunLengths,
-      type: 'histogram',
+    x: maxRunLengths,
+    type: 'histogram',
     };
     const layout = {
-      title: 'Histogram of Maximum Run Lengths',
-      xaxis: { title: 'Maximum Run Length' },
-      yaxis: { title: 'Frequency' },
+    title: 'Histogram of Maximum Run Lengths',
+    xaxis: { title: 'Maximum Run Length' },
+    yaxis: { title: 'Frequency' },
     };
     Plotly.newPlot('plotlyDiv', [trace], layout);
-  }
+}
   
